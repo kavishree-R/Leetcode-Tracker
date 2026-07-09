75489ff80c3c810c1c8f35662e9869d11928b684
Kavishree R <shreekavi649@gmail.com>
2026-07-09 15:12:51 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 3:12:52 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0;
+        for(int e : nums) {
+            ones = ((ones ^ e) & (~ twos));  
+            twos = ((twos ^ e) & (~ ones)); 
+        }
+        return ones;  
+    }
+}
