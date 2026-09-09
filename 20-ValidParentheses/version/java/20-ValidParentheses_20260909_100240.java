f1e6854a9a9f07b8adbe6582e0c5465f16db58eb
Kavishree R <shreekavi649@gmail.com>
2026-09-09 10:02:41 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/version/java/20-ValidParentheses_20260909_100240.java
@@ -0,0 +1,14 @@
+// Last updated: 9/9/2026, 10:02:40 AM
+1class Solution {
+2    public int jump(int[] N) {
+3        int len = N.length - 1, curr = -1, next = 0, ans = 0;
+4        for (int i = 0; next < len; i++) {
+5            if (i > curr) {
+6                ans++;
+7                curr = next;
+8            };
+9            next = Math.max(next, N[i] + i);
+10        };
+11        return ans;
+12    };
+13};
