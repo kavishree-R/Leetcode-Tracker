5f4f0dab4001d572866b20261d8f527202dbae92
Kavishree R <shreekavi649@gmail.com>
2026-09-14 18:11:01 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/version/java/55-JumpGame_20260914_181100.java
@@ -0,0 +1,16 @@
+// Last updated: 9/14/2026, 6:11:00 PM
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int n = nums.length;
+4
+5        int j = n - 1, i = j - 1;
+6
+7        while(j > 0 && i > -1){
+8            if(nums[i] >= j - i) j = i;
+9
+10            i--;
+11        }
+12
+13        return j == 0;
+14    }
+15}
