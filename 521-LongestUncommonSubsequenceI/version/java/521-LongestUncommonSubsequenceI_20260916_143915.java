10e8030667c1b9dff28aa89b138582853899f0cd
Kavishree R <shreekavi649@gmail.com>
2026-09-16 14:39:16 +0530

    Create 521-LongestUncommonSubsequenceI

---
+++ 521-LongestUncommonSubsequenceI/version/java/521-LongestUncommonSubsequenceI_20260916_143915.java
@@ -0,0 +1,10 @@
+// Last updated: 9/16/2026, 2:39:15 PM
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int nmax=nums[n-1]*nums[0]*nums[1];
+        int pmax=nums[n-1]*nums[n-2]*nums[n-3];
+        return Math.max(nmax, pmax);
+    }
+}
